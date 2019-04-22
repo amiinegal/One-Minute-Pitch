ca5549d2e067db1295ca6a90553089843f0efa34
amiinegal <you@example.com>
2019-04-22 21:25:26 +0300

    create a new tablename comments

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from . import db   
+from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
@@ -8,40 +8,40 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
+    # Passes in a user id to this function and the function queries
+    #  the database and gets a user's id as a response
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
-    pitch_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
     pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
-
-
+    comments = db.relationship("Comment", backref="user", lazy="dynamic")
+   
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
-
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'User {self.username}'
 
-
 class Pitch(db.Model):
+
     __tablename__ = 'pitches'
 
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title= db.Column(db.String(300), index=True)
     content = db.Column(db.String(300), index=True)
     category_id = db.Column(db.String)
@@ -59,6 +59,9 @@ class Pitch(db.Model):
     def get_pitches(id):
         pitches = Pitch.query.filter_by(category_id = id).all()
         return pitches
+
+    def __repr__(self):
+        return f"Pitch('{self.id}', '{self.time}')"
 
 class Comment(db.Model):
 
@@ -81,7 +84,4 @@ class Comment(db.Model):
     @classmethod
     def get_comments(cls, id):
         comments = Comment.query.filter_by(pitch_id=id).all()
-        return comments        
-        
-    def __repr__(self):
-        return f'User {self.name}'        
+        return comments
